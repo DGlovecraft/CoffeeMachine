342ae89c9973bba614fe8e6db49d96971548adae
DGlovecraft <nuttawutexp@gmail.com>
2025-02-03 17:10:29 +0700

    Initial commit

---
+++ Form1.Designer.cs
@@ -0,0 +1,429 @@
+﻿namespace WaterShop
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            label1 = new Label();
+            water_ID = new TextBox();
+            coffee_ID = new TextBox();
+            Hot_Milk_ID = new TextBox();
+            label2 = new Label();
+            label3 = new Label();
+            label4 = new Label();
+            label5 = new Label();
+            chocolate_ID = new TextBox();
+            label6 = new Label();
+            label7 = new Label();
+            label8 = new Label();
+            label9 = new Label();
+            groupBox1 = new GroupBox();
+            label10 = new Label();
+            label11 = new Label();
+            label12 = new Label();
+            groupBox2 = new GroupBox();
+            pictureBox4 = new PictureBox();
+            pictureBox1 = new PictureBox();
+            pictureBox3 = new PictureBox();
+            pictureBox2 = new PictureBox();
+            black_button = new Button();
+            americano_button = new Button();
+            mocca_button = new Button();
+            late_button = new Button();
+            label15 = new Label();
+            label13 = new Label();
+            upstock = new Button();
+            groupBox1.SuspendLayout();
+            groupBox2.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)pictureBox4).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox3).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox2).BeginInit();
+            SuspendLayout();
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Font = new Font("Showcard Gothic", 22.2F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            label1.Location = new Point(435, 0);
+            label1.Name = "label1";
+            label1.Size = new Size(320, 46);
+            label1.TabIndex = 1;
+            label1.Text = "Coffee Machine";
+            // 
+            // water_ID
+            // 
+            water_ID.Location = new Point(236, 40);
+            water_ID.Name = "water_ID";
+            water_ID.Size = new Size(125, 28);
+            water_ID.TabIndex = 2;
+            // 
+            // coffee_ID
+            // 
+            coffee_ID.Location = new Point(236, 92);
+            coffee_ID.Name = "coffee_ID";
+            coffee_ID.Size = new Size(125, 28);
+            coffee_ID.TabIndex = 3;
+            // 
+            // Hot_Milk_ID
+            // 
+            Hot_Milk_ID.Location = new Point(236, 147);
+            Hot_Milk_ID.Name = "Hot_Milk_ID";
+            Hot_Milk_ID.Size = new Size(125, 28);
+            Hot_Milk_ID.TabIndex = 4;
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Font = new Font("Goudy Stout", 9F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            label2.Location = new Point(13, 47);
+            label2.Name = "label2";
+            label2.Size = new Size(178, 20);
+            label2.TabIndex = 5;
+            label2.Text = "Water        ";
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.Font = new Font("Goudy Stout", 9F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            label3.Location = new Point(13, 96);
+            label3.Name = "label3";
+            label3.Size = new Size(179, 20);
+            label3.TabIndex = 6;
+            label3.Text = "Coffee        ";
+            // 
+            // label4
+            // 
+            label4.AutoSize = true;
+            label4.Font = new Font("Goudy Stout", 9F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            label4.Location = new Point(8, 209);
+            label4.Name = "label4";
+            label4.Size = new Size(184, 20);
+            label4.TabIndex = 7;
+            label4.Text = "Chocolate ";
+            // 
+            // label5
+            // 
+            label5.AutoSize = true;
+            label5.Font = new Font("Goudy Stout", 9F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            label5.Location = new Point(8, 151);
+            label5.Name = "label5";
+            label5.Size = new Size(175, 20);
+            label5.TabIndex = 8;
+            label5.Text = "HOT MILK  ";
+            // 
+            // chocolate_ID
+            // 
+            chocolate_ID.Location = new Point(236, 202);
+            chocolate_ID.Name = "chocolate_ID";
+            chocolate_ID.Size = new Size(125, 28);
+            chocolate_ID.TabIndex = 9;
+            // 
+            // label6
+            // 
+            label6.AutoSize = true;
+            label6.Font = new Font("Goudy Stout", 9F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            label6.Location = new Point(198, 47);
+            label6.Name = "label6";
+            label6.Size = new Size(23, 20);
+            label6.TabIndex = 10;
+            label6.Text = ": ";
+            // 
+            // label7
+            // 
+            label7.AutoSize = true;
+            label7.Font = new Font("Goudy Stout", 9F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            label7.Location = new Point(198, 99);
+            label7.Name = "label7";
+            label7.Size = new Size(23, 20);
+            label7.TabIndex = 11;
+            label7.Text = ": ";
+            // 
+            // label8
+            // 
+            label8.AutoSize = true;
+            label8.Font = new Font("Goudy Stout", 9F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            label8.Location = new Point(198, 154);
+            label8.Name = "label8";
+            label8.Size = new Size(23, 20);
+            label8.TabIndex = 12;
+            label8.Text = ": ";
+            // 
+            // label9
+            // 
+            label9.AutoSize = true;
+            label9.Font = new Font("Goudy Stout", 9F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            label9.Location = new Point(198, 209);
+            label9.Name = "label9";
+            label9.Size = new Size(23, 20);
+            label9.TabIndex = 13;
+            label9.Text = ": ";
+            // 
+            // groupBox1
+            // 
+            groupBox1.Controls.Add(label9);
+            groupBox1.Controls.Add(label8);
+            groupBox1.Controls.Add(label7);
+            groupBox1.Controls.Add(label6);
+            groupBox1.Controls.Add(chocolate_ID);
+            groupBox1.Controls.Add(label5);
+            groupBox1.Controls.Add(label4);
+            groupBox1.Controls.Add(label3);
+            groupBox1.Controls.Add(label2);
+            groupBox1.Controls.Add(Hot_Milk_ID);
+            groupBox1.Controls.Add(coffee_ID);
+            groupBox1.Controls.Add(water_ID);
+            groupBox1.Font = new Font("Palatino Linotype", 9F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            groupBox1.Location = new Point(22, 49);
+            groupBox1.Name = "groupBox1";
+            groupBox1.Size = new Size(423, 304);
+            groupBox1.TabIndex = 14;
+            groupBox1.TabStop = false;
+            groupBox1.Text = "ingredient";
+            // 
+            // label10
+            // 
+            label10.AutoSize = true;
+            label10.Font = new Font("MV Boli", 18F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            label10.Location = new Point(39, 30);
+            label10.Name = "label10";
+            label10.Size = new Size(184, 40);
+            label10.TabIndex = 15;
+            label10.Text = "Coffee_Late";
+            // 
+            // label11
+            // 
+            label11.AutoSize = true;
+            label11.Font = new Font("MV Boli", 18F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            label11.Location = new Point(39, 348);
+            label11.Name = "label11";
+            label11.Size = new Size(264, 40);
+            label11.TabIndex = 16;
+            label11.Text = "Coffee_Americano";
+            // 
+            // label12
+            // 
+            label12.AutoSize = true;
+            label12.Font = new Font("MV Boli", 18F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            label12.Location = new Point(39, 189);
+            label12.Name = "label12";
+            label12.Size = new Size(205, 40);
+            label12.TabIndex = 17;
+            label12.Text = "Coffee_Mocca";
+            // 
+            // groupBox2
+            // 
+            groupBox2.Controls.Add(pictureBox4);
+            groupBox2.Controls.Add(pictureBox1);
+            groupBox2.Controls.Add(pictureBox3);
+            groupBox2.Controls.Add(pictureBox2);
+            groupBox2.Controls.Add(black_button);
+            groupBox2.Controls.Add(americano_button);
+            groupBox2.Controls.Add(mocca_button);
+            groupBox2.Controls.Add(late_button);
+            groupBox2.Controls.Add(label15);
+            groupBox2.Controls.Add(label12);
+            groupBox2.Controls.Add(label11);
+            groupBox2.Controls.Add(label10);
+            groupBox2.Font = new Font("Palatino Linotype", 9F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            groupBox2.Location = new Point(752, 49);
+            groupBox2.Name = "groupBox2";
+            groupBox2.Size = new Size(484, 616);
+            groupBox2.TabIndex = 25;
+            groupBox2.TabStop = false;
+            groupBox2.Text = "MACHINE";
+            // 
+            // pictureBox4
+            // 
+            pictureBox4.Image = (Image)resources.GetObject("pictureBox4.Image");
+            pictureBox4.Location = new Point(331, 483);
+            pictureBox4.Name = "pictureBox4";
+            pictureBox4.Size = new Size(136, 127);
+            pictureBox4.SizeMode = PictureBoxSizeMode.StretchImage;
+            pictureBox4.TabIndex = 35;
+            pictureBox4.TabStop = false;
+            pictureBox4.UseWaitCursor = true;
+            // 
+            // pictureBox1
+            // 
+            pictureBox1.Image = (Image)resources.GetObject("pictureBox1.Image");
+            pictureBox1.Location = new Point(331, 14);
+            pictureBox1.Name = "pictureBox1";
+            pictureBox1.Size = new Size(136, 137);
+            pictureBox1.SizeMode = PictureBoxSizeMode.StretchImage;
+            pictureBox1.TabIndex = 34;
+            pictureBox1.TabStop = false;
+            pictureBox1.UseWaitCursor = true;
+            // 
+            // pictureBox3
+            // 
+            pictureBox3.Image = (Image)resources.GetObject("pictureBox3.Image");
+            pictureBox3.Location = new Point(331, 168);
+            pictureBox3.Name = "pictureBox3";
+            pictureBox3.Size = new Size(136, 127);
+            pictureBox3.SizeMode = PictureBoxSizeMode.StretchImage;
+            pictureBox3.TabIndex = 33;
+            pictureBox3.TabStop = false;
+            pictureBox3.UseWaitCursor = true;
+            // 
+            // pictureBox2
+            // 
+            pictureBox2.Image = (Image)resources.GetObject("pictureBox2.Image");
+            pictureBox2.Location = new Point(331, 328);
+            pictureBox2.Name = "pictureBox2";
+            pictureBox2.Size = new Size(136, 137);
+            pictureBox2.SizeMode = PictureBoxSizeMode.StretchImage;
+            pictureBox2.TabIndex = 32;
+            pictureBox2.TabStop = false;
+            pictureBox2.UseWaitCursor = true;
+            // 
+            // black_button
+            // 
+            black_button.Location = new Point(43, 541);
+            black_button.Name = "black_button";
+            black_button.Size = new Size(118, 42);
+            black_button.TabIndex = 30;
+            black_button.Text = "Choose";
+            black_button.UseVisualStyleBackColor = true;
+            black_button.Click += black_button_Click;
+            // 
+            // americano_button
+            // 
+            americano_button.Location = new Point(43, 391);
+            americano_button.Name = "americano_button";
+            americano_button.Size = new Size(118, 42);
+            americano_button.TabIndex = 29;
+            americano_button.Text = "Choose";
+            americano_button.UseVisualStyleBackColor = true;
+            americano_button.Click += americano_button_Click;
+            // 
+            // mocca_button
+            // 
+            mocca_button.Location = new Point(43, 242);
+            mocca_button.Name = "mocca_button";
+            mocca_button.Size = new Size(118, 42);
+            mocca_button.TabIndex = 28;
+            mocca_button.Text = "Choose";
+            mocca_button.UseVisualStyleBackColor = true;
+            mocca_button.Click += mocca_button_Click;
+            // 
+            // late_button
+            // 
+            late_button.Location = new Point(43, 77);
+            late_button.Name = "late_button";
+            late_button.Size = new Size(118, 42);
+            late_button.TabIndex = 27;
+            late_button.Text = "Choose";
+            late_button.UseVisualStyleBackColor = true;
+            late_button.Click += late_button_Click;
+            // 
+            // label15
+            // 
+            label15.AutoSize = true;
+            label15.Font = new Font("MV Boli", 18F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            label15.Location = new Point(43, 489);
+            label15.Name = "label15";
+            label15.Size = new Size(188, 40);
+            label15.TabIndex = 25;
+            label15.Text = "Coffee_Black";
+            // 
+            // label13
+            // 
+            label13.AutoSize = true;
+            label13.Font = new Font("MV Boli", 18F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            label13.Location = new Point(732, 434);
+            label13.Name = "label13";
+            label13.Size = new Size(0, 40);
+            label13.TabIndex = 25;
+            // 
+            // upstock
+            // 
+            upstock.Location = new Point(30, 359);
+            upstock.Name = "upstock";
+            upstock.Size = new Size(118, 42);
+            upstock.TabIndex = 26;
+            upstock.Text = "Up Stock + 500";
+            upstock.UseVisualStyleBackColor = true;
+            upstock.Click += upstock_Click;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(1282, 682);
+            Controls.Add(upstock);
+            Controls.Add(label13);
+            Controls.Add(groupBox2);
+            Controls.Add(groupBox1);
+            Controls.Add(label1);
+            Name = "Form1";
+            Text = "Form1";
+            groupBox1.ResumeLayout(false);
+            groupBox1.PerformLayout();
+            groupBox2.ResumeLayout(false);
+            groupBox2.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)pictureBox4).EndInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).EndInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox3).EndInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox2).EndInit();
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+        private Label label1;
+        private TextBox water_ID;
+        private TextBox coffee_ID;
+        private TextBox Hot_Milk_ID;
+        private Label label2;
+        private Label label3;
+        private Label label4;
+        private Label label5;
+        private TextBox chocolate_ID;
+        private Label label6;
+        private Label label7;
+        private Label label8;
+        private Label label9;
+        private GroupBox groupBox1;
+        private Label label10;
+        private Label label11;
+        private Label label12;
+        private GroupBox groupBox2;
+        private Label label13;
+        private Button upstock;
+        private Label label15;
+        private Button black_button;
+        private Button americano_button;
+        private Button mocca_button;
+        private Button late_button;
+        private PictureBox pictureBox4;
+        private PictureBox pictureBox1;
+        private PictureBox pictureBox3;
+        private PictureBox pictureBox2;
+    }
+}
